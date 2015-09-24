d9cea0948ee267af0bbe7e9bb9622082b0a583d4
unknown <wangso@SMHPC15407.SMH.SMHROOT.NET>
2015-09-24 15:47:46 -0400

    update routeProvider

---
+++ app.js
@@ -0,0 +1,20 @@
+angular.module('Ins', ['ngRoute', 'ngMessages']).config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    })
+    .when('/signup', {
+      templateUrl: 'views/signup.html',
+      controller: 'SignupCtrl'
+    })
+    .when('/photo/:id', {
+      templateUrl: 'views/detail.html',
+      controller: 'DetailCtrl'
+    })
+    .otherwise('/');
+});
