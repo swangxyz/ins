9f8fa3b52cd7edf319affb10b0c82c2d195f7b40
unknown <wangso@SMHPC15407.SMH.SMHROOT.NET>
2015-09-25 16:26:20 -0400

    Finish login view

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
-angular.module('Ins', ['ngRoute', 'ngMessages']).config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'view/home.html',
-      controller: 'HomeCtrl'
-    })
-    .when('/login', {
-      templateUrl: 'view/login.html',
-      controller: 'LoginCtrl'
-    })
-    .when('/signup', {
-      templateUrl: 'view/signup.html',
-      controller: 'SignupCtrl'
-    })
-    .when('/photo/:id', {
-      templateUrl: 'view/detail.html',
-      controller: 'DetailCtrl'
-    })
-    .otherwise('/');
-});
+angular.module('Ins', ['ngRoute', 'ngMessages', 'satellizer', 'environment'])
+    .config(function($routeProvider, envServiceProvider, $authProvider) {
+        envServiceProvider.config({
+            domains: {
+                development: ['localhost:8080', 'dev.local'],
+                production: ['', '', '']
+                    // anotherStage: []
+            },
+            vars: {
+                development: {
+                    serverUrl: '//localhost:8080',
+                    apiUrl: '//localhost/api',
+                    staticUrl: '//localhost/static'
+                        // anotherCustomVar: ''
+                },
+                production: {
+                    serverUrl: '//localhost:8080',
+                    apiUrl: '//',
+                    staticUrl: '//'
+                        // anotherCustomVar: ''
+                }
+            }
+        });
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'view/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'view/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/signup', {
+                templateUrl: 'view/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .when('/photo/:id', {
+                templateUrl: 'view/detail.html',
+                controller: 'DetailCtrl'
+            })
+            .otherwise('/');
+
+        envServiceProvider.set('development');
+        //envService.set('production');
+        $serverUrl = envServiceProvider.read('serverUrl')
+
+        $authProvider.loginUrl = $serverUrl + '/auth/login';
+        $authProvider.signupUrl = $serverUrl + 'auth/signup';
+        $authProvider.oauth2({
+            name: 'interest',
+            url: 'http://localhost:8080/auth/instagram',
+            redirectUri: 'http://localhost:3000',
+            requiredUrlParams: ['scope'],
+            scope: ['likes'],
+            scopeDelimiter: '+',
+            authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+        });
 
 
+    });
--- controller/home.js
+++ controller/home.js
@@ -1,8 +1,11 @@
-angular.module('Ins').controller('HomeCtrl', function($scope){
+angular.module('Ins').controller('HomeCtrl', function($scope,$window,$rootScope,$auth){
 	$scope.isAuthenticated = function(){
-		
+		return $auth.isAuthenticated();
 	};
 	$scope.linkInstagram = function(){
-		
+		$auth.link('Instagram').then(function(response) {
+			$window.localStorage.currentUser = JSON.stringify(response.data.user);
+			$rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+		})
 	};
 });
